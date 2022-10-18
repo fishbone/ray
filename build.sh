ecbea3613679a69a97a40dc535b2a131e0a87160
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2022-10-18 12:43:44 -0700

    update build

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "${OSTYPE}" = msys ]; then
-  echo "WARNING: ${0##*/} is not recommended on MSYS2, as MSYS2 alters the build environment."
-fi
 
 if [ -z "${PYTHON3_BIN_PATH-}" ]; then
   PYTHON3_BIN_PATH="$(command -v python3 || command -v python || echo python)"
