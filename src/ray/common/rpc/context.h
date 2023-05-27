07154eb337cc8fced952025ce21474e9330202e1
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-05-27 00:34:25 +0000

    fix Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- src/ray/common/rpc/context.h
+++ src/ray/common/rpc/context.h
@@ -7,7 +7,21 @@
 
 namespace ray {
 namespace rpc {
+/*
+template<typename CompletionToken, typename ... Args>
+auto MakeCallback(CompletionToken&& token, Args &&args...) {
+  return [&&token = std::forward<CompletionToken>(token),
+          args = std::make_tuple(std::forward<Args>(args) ...)]() mutable {
+    auto alloc = boost::asio::get_associated_allocator(token, boost::asio::recycling_allocator<void>());
+    auto ex = boost::asio::get_associated_executor(token);
+    RAY_CHECK(ex) << "Invalid executor";
+    auto completion = [
+    boost::asio::dispatch(
+        ex,
 
+  }
+}
+*/
 template <typename Request,
           typename Response,
           template <typename, typename>
@@ -16,43 +30,42 @@ class RpcReactorBase : public Reactor<Request, Response> {
   using Base = Reactor<Request, Response>;
 
  public:
-  template <typename CompletionToken>
-  auto Read(Request &request, CompletionToken &&token) {
-    auto init = [this](auto &&handler, Request *request) mutable {
-      RAY_CHECK(!read_callback_);
-      Base::StartRead(request);
+  template <typename CompletionToken, typename R>
+  auto Read(R &request, CompletionToken &&token) {
+    RAY_CHECK(!read_callback_);
+    template<typename C>
+    auto init = [this](C &&handler, auto *request) mutable {
       read_callback_ = [handler =
                             std::forward<decltype(handler)>(handler)](bool ok) mutable {
         auto alloc = boost::asio::get_associated_allocator(
             handler, boost::asio::recycling_allocator<void>());
         auto ex = boost::asio::get_associated_executor(handler);
-        // RAY_CHECK(ex) << "Invalid executor";
+        RAY_CHECK(ex) << "Invalid executor";
         boost::asio::dispatch(
             ex,
             boost::asio::bind_allocator(
                 alloc,
-                [handler = std::forward<decltype(handler)>(handler), ok]() mutable {
-                  std::forward<decltype(handler)>(handler)(ok);
+                [handler = std::forward<C>(handler), ok]() mutable {
+                  std::forward<C>(handler)(ok);
                 }));
       };
+      Base::StartRead(request);
     };
 
     return boost::asio::async_initiate<CompletionToken, void(bool)>(
         init, std::forward<CompletionToken>(token), &request);
   }
 
-  template <typename CompletionToken>
-  auto Write(const Response &response, CompletionToken &&token) {
+  template <typename CompletionToken, typename R>
+  auto Write(const R &response, CompletionToken &&token) {
     return Write(response, grpc::WriteOptions(), std::move(token));
   }
 
-  template <typename CompletionToken>
-  auto Write(const Response &response, grpc::WriteOptions opts, CompletionToken &&token) {
+  template <typename CompletionToken, typename R>
+  auto Write(const R &response, grpc::WriteOptions opts, CompletionToken &&token) {
+    RAY_CHECK(!write_callback_);
     auto init = [this, opts = std::move(opts)](auto &&handler,
-                                               const Response &response) mutable {
-      RAY_CHECK(!write_callback_);
-      Base::StartWrite(&response, opts);
-
+                                               const auto &response) mutable {
       write_callback_ = [handler =
                              std::forward<decltype(handler)>(handler)](bool ok) mutable {
         auto alloc = boost::asio::get_associated_allocator(
@@ -67,6 +80,7 @@ class RpcReactorBase : public Reactor<Request, Response> {
                   std::forward<decltype(handler)>(handler)(ok);
                 }));
       };
+      Base::StartWrite(&response, opts);
     };
 
     return boost::asio::async_initiate<CompletionToken, void(bool)>(
@@ -76,36 +90,21 @@ class RpcReactorBase : public Reactor<Request, Response> {
  private:
   void OnReadDone(bool ok) override {
     if (read_callback_) {
-      std::move(read_callback_)(ok);
+      auto f = std::move(read_callback_);
+      f(ok);
     }
   }
 
   void OnWriteDone(bool ok) override {
     if (write_callback_) {
-      std::move(write_callback_)(ok);
+      auto f = std::move(write_callback_);
+      f(ok);
     }
   }
 
  private:
   fu2::unique_function<void(bool ok)> read_callback_;
   fu2::unique_function<void(bool ok)> write_callback_;
-};
-
-template <typename Request,
-          typename Response,
-          template <typename, typename>
-          typename Reactor>
-class ClientRpcReactor : private RpcReactorBase<Request, Response, Reactor> {
-  using Base = RpcReactorBase<Request, Response, Reactor>;
-
- public:
-  grpc::ClientContext &GetContext() { return client_context_; }
-
-  void Start() { Base::StartCall(); }
-
- private:
-  void OnDone(const grpc::Status &) override { delete this; }
-  grpc::ClientContext client_context_;
 };
 
 template <typename Request,
@@ -119,14 +118,53 @@ class ServerRpcReactor : public RpcReactorBase<Request, Response, Reactor> {
       : server_context_(server_context) {}
 
   static void operator delete(void *ptr) {
+    RAY_LOG(DEBUG) << "ServerRPC delete";
     auto self = (Self *)(ptr);
     self->Finish(self->status_);
   }
 
  private:
-  void OnDone() { ::operator delete(this); }
+  void OnDone() {
+    RAY_LOG(DEBUG) << "ServerRPC OnDone";
+    ::operator delete(this);
+  }
   grpc::CallbackServerContext *server_context_;
   grpc::Status status_;
+};
+
+template<typename Request, typename Response, template<typename, typename> typename Reactor>
+class ClientRpcReactor : public RpcReactorBase<Request, Response, Reactor> {
+ public:
+  using Base = RpcReactorBase<Request, Response, Reactor>;
+  using Self = ClientRpcReactor<Request, Response, Reactor>;
+
+  ClientRpcReactor() {}
+
+  grpc::ClientContext& GetContext() {
+    return client_context_;
+  }
+
+  void Start() {
+    RAY_LOG(DEBUG) << "StartCall";
+    Base::StartCall();
+    Base::AddHold();
+  }
+
+  static void operator delete(void* ptr) {
+    RAY_LOG(DEBUG) << "ClientRPC delete";
+  }
+
+  // template<typename CompletionToken>
+  // auto Finish(CompletionToken &&token) {
+
+  // }
+ private:
+  void OnDone(const grpc::Status &status) override {
+    RAY_LOG(DEBUG) << "ClientRPC OnDone";
+    ::operator delete(this);
+  }
+
+  grpc::ClientContext client_context_;
 };
 
 }  // namespace rpc
--- src/ray/common/rpc/e2e.cc
+++ src/ray/common/rpc/e2e.cc
@@ -15,30 +15,32 @@ class TestStreamingServiceImpl final : public TestStreamingService::CallbackServ
  public:
   grpc::ServerBidiReactor<PingRequest, PingReply> *Ping(
       grpc::CallbackServerContext *context) override {
+    RAY_LOG(INFO) << "Server: Ping request";
     auto reactor = std::make_shared<
         ServerRpcReactor<PingRequest, PingReply, grpc::ServerBidiReactor>>(context);
-    auto run = [reactor]() -> boost::asio::awaitable<void> {
+    auto run = [](auto reactor) -> boost::asio::awaitable<void> {
       PingRequest request;
       while (co_await reactor->Read(request, boost::asio::use_awaitable)) {
+        RAY_LOG(INFO) <<"ServerRead: " << request.cnt();
         PingReply reply;
+        reply.set_cnt(request.cnt());
         if (!co_await reactor->Write(reply, boost::asio::use_awaitable)) {
           break;
         }
+        RAY_LOG(INFO) <<"ServerWrite: " << reply.cnt();
       }
+      RAY_LOG(INFO) << "Server: EOF";
     };
-
-    boost::asio::co_spawn(io_context.get_executor(), run(), boost::asio::detached);
+    boost::asio::co_spawn(io_context.get_executor(), run(reactor), boost::asio::detached);
     return reactor.get();
   }
-
- private:
 };
 
 std::unique_ptr<std::thread> StartServer() {
   auto t = std::make_unique<std::thread>([]() {
     TestStreamingServiceImpl service;
     grpc::ServerBuilder builder;
-    builder.AddListeningPort("localhost:8090", grpc::InsecureServerCredentials());
+    builder.AddListeningPort("127.0.0.1:8090", grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     auto server = builder.BuildAndStart();
     server->Wait();
@@ -46,12 +48,46 @@ std::unique_ptr<std::thread> StartServer() {
   return t;
 }
 
+std::unique_ptr<std::thread> StartClient() {
+  auto t = std::make_unique<std::thread>([]() {
+    RAY_LOG(INFO) << "Create channel";
+    auto channel = grpc::CreateChannel("127.0.0.1:8090", grpc::InsecureChannelCredentials());
+    auto stub = TestStreamingService::NewStub(channel);
+    auto reactor = std::make_shared<ClientRpcReactor<PingRequest, PingReply, grpc::ClientBidiReactor>>();
+
+    stub->async()->Ping(&reactor->GetContext(), reactor.get());
+    reactor->Start();
+    auto run = [](auto reactor) -> boost::asio::awaitable<void> {
+      int64_t i = 0;
+      PingRequest request;
+      request.set_cnt(i++);
+      PingReply reply;
+      while (co_await reactor->Write(request, boost::asio::use_awaitable)) {
+        RAY_LOG(INFO) << "ClientWrite: " << request.cnt();
+        if (!co_await reactor->Read(reply, boost::asio::use_awaitable)) {
+          RAY_LOG(INFO) << "ClientRead: EOF";
+          break;
+        }
+        RAY_LOG(INFO) << "ClientRead: " << reply.cnt();
+        request.set_cnt(i++);
+      }
+      RAY_LOG(INFO) << "Client: EOF";
+    };
+    boost::asio::co_spawn(io_context.get_executor(), run(reactor), boost::asio::detached);
+  });
+  return t;
+}
+
 int main() {
   auto server = StartServer();
+  auto client = StartClient();
   auto t = std::make_unique<std::thread>([]() {
     boost::asio::io_context::work work(io_context);
     io_context.run();
   });
+
   t->join();
+  server->join();
+  client->join();
   return 0;
 }
