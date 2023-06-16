dcdb8b9b965d4ff4872031aa5d1172206bd8bd7a
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-06-16 14:57:42 -0700

    format

--- src/ray/object_manager/test/external_storage_test.cc
+++ src/ray/object_manager/test/external_storage_test.cc
@@ -2,11 +2,11 @@
 #include "ray/object_manager/external_storage/base.h"
 
 TEST(ExternalStorageTest, Test) {
-  ray::object_store::ExternalStorage store;
-  ASSERT_TRUE(store.AsyncWriteSome("A", std::string("B"), boost::asio::use_future)
+  ray::object_store::File file;
+  ASSERT_TRUE(file.AsyncWriteSome("A", std::string("B"), boost::asio::use_future)
                   .get()
                   .IsNotImplemented());
-  ASSERT_TRUE(store.AsyncReadSome("A", std::string(), boost::asio::use_future)
+  ASSERT_TRUE(file.AsyncReadSome("A", std::string(), boost::asio::use_future)
                   .get()
                   .IsNotImplemented());
 }
